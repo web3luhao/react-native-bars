1b8134824d57990b7997dcfa4d8699c86dbe03cc
web3luhao <luhao@web3.com>
2022-01-10 15:57:54 +0800

    color changed by setSoftInputMode

--- example/android/app/src/main/java/com/rnbarsexample/MainActivity.java
+++ example/android/app/src/main/java/com/rnbarsexample/MainActivity.java
@@ -1,10 +1,33 @@
 package com.rnbarsexample;
 
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.zoontek.rnbars.RNBars;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    new Timer().schedule(new TimerTask() {
+      @Override
+      public void run() {
+        // here goes your code to delay
+        runOnUiThread(() -> {
+          Window window = getWindow();
+          if (window != null) {
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+          }
+        });
+      }
+    }, 4000L);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
